ba0f617ae75f7fcabd1590f50a31d6e6711b8c91
Andressaggudelo <cagudeloa@uninorte.edu.co>
2019-08-29 16:49:21 -0500

    Translation, Rotation, and Scaling working but some deformations are happening.

--- src/keylistenerexample/KeyListenerExample.java
+++ src/keylistenerexample/KeyListenerExample.java
@@ -26,18 +26,21 @@ public class KeyListenerExample extends JPanel implements KeyListener {
     public static final int FRAME_WIDTH = 500;
     public static final int FRAME_HEIGHT = 500;
     
+//    Point points[]={new Point(200, 200),
+//                    new Point(300, 200), new Point(300, 300), new Point(250, 150), new Point(200, 300)};
+//    Point edges[]={new Point(0,1), new Point(1,2), new Point(0,3), new Point(2,4), new Point(4,0), new Point(3,1)};
     
-    Point points[]={new Point(200, 200),
-                    new Point(300, 200),
-                    new Point(300, 300),
-                    new Point(250, 350),
-                    new Point(200, 300)};
-    Point edges[]={new Point(0,1),
-                    new Point(1,2),
-                    new Point(2,3),
-                    new Point(2,4),
-                    new Point(4,0),
-                    new Point(3,4)};
+    Point points[]={new Point(0, 0),
+                    new Point(100, 0), 
+                    new Point(100, 100), 
+                    new Point(50, -50), 
+                    new Point(0, 100)};
+    Point edges[]={new Point(0,1), 
+                    new Point(1,2), 
+                    new Point(0,3), 
+                    new Point(2,4), 
+                    new Point(4,0), 
+                    new Point(3,1)};
     
     public KeyListenerExample() {
         // El panel, por defecto no es "focusable". 
@@ -52,14 +55,10 @@ public class KeyListenerExample extends JPanel implements KeyListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         
-            readfile(g);
-    }
-
-    
-    private void readfile(Graphics g){
         for (Point edge : edges) {
-            g.drawLine(points[edge.x].x, points[edge.x].y, points[edge.y].x, points[edge.y].y);
+            g.drawLine(200+points[edge.x].x, 200+points[edge.x].y, 200+points[edge.y].x, 200+points[edge.y].y);
         }
+        
     }
     
     
@@ -89,6 +88,32 @@ public class KeyListenerExample extends JPanel implements KeyListener {
             case KeyEvent.VK_S:
                 for (Point point : points) {
                     point.y += 10;
+                }
+                break;
+            case KeyEvent.VK_Z: // Reduce image size.
+                for (Point point : points) {
+                    point.x *= 0.9;
+                    point.y *= 0.9;
+                }
+                break;
+            case KeyEvent.VK_X: // Increase image size.
+                for (Point point : points) {
+                    point.x *= 1.1;
+                    point.y *= 1.1;
+                }
+                break;
+            case KeyEvent.VK_N: // Reduce image size.
+                int angle = 20;
+                for (Point point : points) {
+                    point.x = (int)(point.x*Math.cos(Math.toRadians(angle))-point.y*Math.sin(Math.toRadians(angle)));
+                    point.y = (int)(point.x*Math.sin(Math.toRadians(angle))+point.y*Math.cos(Math.toRadians(angle)));
+                }
+                break;
+            case KeyEvent.VK_M: // Increase image size.
+                angle = -20;
+                for (Point point : points) {
+                    point.x = (int)(point.x*Math.cos(Math.toRadians(angle))-point.y*Math.sin(Math.toRadians(angle)));
+                    point.y = (int)(point.x*Math.sin(Math.toRadians(angle))+point.y*Math.cos(Math.toRadians(angle)));
                 }
                 break;
             default:
